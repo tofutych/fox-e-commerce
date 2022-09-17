b53d87df90815afc694a6e31f9882cca5f1a3088
tofut <martirosov.davidka@mail.ru>
2022-09-18 01:20:51 +0300

    new "Product" model

--- product/models.py
+++ product/models.py
@@ -1,4 +1,8 @@
+from django.core.files import File
 from django.db import models
+
+from io import BytesIO
+from PIL import Image
 
 
 class Category(models.Model):
@@ -13,3 +17,53 @@ class Category(models.Model):
 
     def get_absolute_url(self):
         return f"/{self.slug}/"
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(upload_to='uploads/', blank=True, null=True)
+    thumbnail = models.ImageField(upload_to='uploads/', blank=True, null=True)
+    date_added = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('-date_added',)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f"/{self.category.slug}/{self.slug}/"
+
+    def get_image(self):
+        if self.image:
+            return 'http://127.0.0.1:8000' + self.image.url
+        return ''
+
+    def get_thumbnail(self):
+        if self.thumbnail:
+            return 'http://127.0.0.1:8000' + self.thumbnail.url
+        else:
+            if self.image:
+                self.thumbnail = self.make_thumbnail(self.image)
+                self.save()
+
+                return 'http://127.0.0.1:8000' + self.thumbnail.url
+            else:
+                return ''
+
+    @staticmethod
+    def make_thumbnail(image, size=(300, 200)):
+        img = Image.open(image)
+        img.convert('RGB')
+        img.thumbnail(size)
+
+        thumb_io = BytesIO()
+        img.save(thumb_io, 'JPEG', quality=85)
+
+        thumbnail = File(thumb_io, name=image.name)
+
+        return thumbnail
