c0ca97db2e10b0d53fcfd98b246f1d70ccebded9
tofut <martirosov.davidka@mail.ru>
2022-09-18 03:08:02 +0300

    some api

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Product
+
+
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
