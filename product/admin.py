4cb786b6b93ff48378f07c638659cde7e125aa51
tofut <martirosov.davidka@mail.ru>
2022-09-19 23:18:41 +0300

    some admin settings

--- product/admin.py
+++ product/admin.py
@@ -5,10 +5,14 @@ from .models import Category, Product
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'date_added')
+    list_filter = ('date_added',)
+    search_fields = ('id', 'name')
     prepopulated_fields = {"slug": ("name",)}
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
     prepopulated_fields = {"slug": ("name",)}
 
