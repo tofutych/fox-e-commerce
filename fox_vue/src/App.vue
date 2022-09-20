7c6d68d50f2175403429f993b441f9685268a5ab
tofut <martirosov.davidka@mail.ru>
2022-09-20 03:30:55 +0300

    foxinisation

--- fox_vue/src/App.vue
+++ fox_vue/src/App.vue
@@ -101,7 +101,7 @@ export default {
   margin: 4px;
   border-radius: 50%;
   border: 6px solid #ccc;
-  border-color: #cccccc transparent #cccccc transparent;
+  border-color: hsl(44deg, 100%, 77%) transparent hsl(44deg, 100%, 77%) transparent;
   animation: lds-dual-ring 1.2s linear infinite;
 }
 
@@ -124,4 +124,29 @@ export default {
     height: 40px;
   }
 }
+
+$navbar-item-color: lighten($text, 30%);
+</style>
+
+<style lang="css">
+.router-link-exact-active.navbar-item {
+  -webkit-animation: 0.3s highlight 0.3s 1 normal forwards;
+  animation: 0.3s highlight 0.3s 1 normal forwards;
+  background-image: linear-gradient(90deg, hsl(44deg, 100%, 77%) 50%, rgba(255, 255, 255, 0) 50%);
+  background-size: 200% 100%;
+  background-position: 100% 0;
+}
+
+@-webkit-keyframes highlight {
+  to {
+    background-position: 0 0;
+  }
+}
+
+@keyframes highlight {
+  to {
+    background-position: 0 0;
+  }
+}
+
 </style>
